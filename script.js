4b281c6901d73e1343816f0035544e136b3885af
Misha <bruh@example.com>
2020-09-25 00:35:54 +0500

    Version2 initial. Made query work. Got to improve table

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
-
-/* $(document).on('keyup', '#input', function (){//make it parametric
-    var url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address";
-    var token = "6aa3e113e95eb7fff794dec3f36b26b1fce6c145";
-    var query = "спб ботаничес";
-
-    var options = {
-        method: "POST",
-        mode: "cors",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-            "Authorization": "Token " + token
-        },
-        body: JSON.stringify({query: query})
-    }
-
-    //fetch(url, options)
-    //.then(response => response.text())
-    //.then(result => console.log(result))
-    //.catch(error => console.log("error", error));
-}); */
-
 var token = "6aa3e113e95eb7fff794dec3f36b26b1fce6c145";
+var url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address";
 
 $("#address").suggestions({
     token: token,
@@ -30,4 +8,39 @@ $("#address").suggestions({
     onSelect: function(suggestion) {
         console.log(suggestion);
     }
+});
+
+$(document).ready(function(){
+
+    var table = document.getElementById("suggestions_table");
+
+    function show(result){
+        //console.log(result.suggestions[0]);
+        result.suggestions.forEach(element => {
+            //console.log(element.value);
+            var new_row = table.insertRow();
+            new_row.innerHTML = element.value;
+        });
+
+    }
+
+    $('#inp').on('keyup',function(){
+        var query = $('#inp').val();
+        var options = {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": "Token " + token
+            },
+            body: JSON.stringify({query: query})
+        }
+    
+        fetch(url, options)
+        .then(response => response.json())
+        .then(result => show(result))
+        .catch(error => console.log("error", error));
+    })
+    
 });
