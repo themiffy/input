27b47283cc4f8d0ae2bf41810a5d18cf252935aa
Misha <bruh@example.com>
2020-09-27 23:35:41 +0500

    Made show() function work. Added styles for the table

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var token = "6aa3e113e95eb7fff794dec3f36b26b1fce6c145";
 var url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address";
 
+
 $("#address").suggestions({
     token: token,
     type: "ADDRESS",
@@ -14,17 +15,29 @@ $(document).ready(function(){
 
     var table = document.getElementById("suggestions_table");
 
+    table.onclick = function(event) {
+        let target = event.target;
+        if (target.tagName != 'TR') return;
+        $('#inp').val(target.innerHTML);
+        $('#inp').keyup();
+    };
+
     function show(result){
-        //console.log(result.suggestions[0]);
+
         result.suggestions.forEach(element => {
             //console.log(element.value);
             var new_row = table.insertRow();
             new_row.innerHTML = element.value;
+            table.style.display = "block"; //show the table
         });
 
     }
 
     $('#inp').on('keyup',function(){
+        
+        table.style.display = "none"; //hiding the table
+        $("#suggestions_table tr").remove();// Clearing the table
+        
         var query = $('#inp').val();
         var options = {
             method: "POST",
@@ -39,7 +52,7 @@ $(document).ready(function(){
     
         fetch(url, options)
         .then(response => response.json())
-        .then(result => show(result))
+        .then(result => show(result)) // Result goes into a function
         .catch(error => console.log("error", error));
     })
     
